affa69f3be3d43bebd2989b36d81277e0b0a7dda
Avishka-Perera <pereramat2000@gmail.com>
2022-04-27 13:17:55 +0530

    utilities for modal

---
+++ src/main/webapp/js/utilities.js
@@ -0,0 +1,56 @@
+const displayModel = (title_text, message_text) => {
+    const root = document.createElement("DIV");
+    const modal = document.createElement("DIV");
+    const title = document.createElement("H3");
+    const message = document.createElement("P");
+    const button = document.createElement("BUTTON");
+
+    title.innerText = title_text;
+    message.innerText = message_text;
+    button.innerText = "CLOSE";
+
+    root.className = "backdrop-overlay";
+    modal.className = "modal-body";
+    title.className = "modal-heading";
+    message.className = "modal-message";
+    button.className = "modal-button";
+
+    button.onclick = () => {
+        root.remove();
+    };
+
+    modal.appendChild(title);
+    modal.appendChild(message);
+    modal.appendChild(button);
+    root.appendChild(modal);
+
+    document.body.appendChild(root);
+}
+
+const validateEmail = (mail) => {
+    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(mail);
+}
+
+const validateUserFormData = () => {
+    const name = document.querySelector("#addUserForm #name").value;
+    const email = document.querySelector("#addUserForm #email").value;
+    const password = document.querySelector("#addUserForm #password").value;
+    const confirmPassword = document.querySelector("#addUserForm #confirm-password").value;
+    const contactNumber = document.querySelector("#addUserForm #contactNumber").value;
+
+    console.log(email, password, confirmPassword);
+
+    if (password !== confirmPassword) {
+        document.querySelector("#addUserForm #confirm-password").value = "";
+        return ["Password Error", "Passwords do not match each other. Please re-enter"];
+    }
+    if (password.length < 8) return ["Password Error", "Weak password. Please enter a password with 8 or more charachters"];
+
+    if (!validateEmail(email)) return ["Email error", "Invalid email. Please enter a valid email"];
+
+    if (name === "") return ["Name error", "Name should be none empty"];
+
+    if (contactNumber === "") return ["Contact number error", "Contact number should be none empty"];
+
+    return ["Valid", ""];
+}
